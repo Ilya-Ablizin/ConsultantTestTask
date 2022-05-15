21209bfc2fade4b517f7467b03960e09cf72cc63
Ilya-Ablizin <iablizinwork@gmail.com>
2022-05-15 14:26:55 +0300

    [first_version] new_project

---
+++ src/test/java/ConsultantTest.java
@@ -0,0 +1,29 @@
+import Settings.WebDriverSettings;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class ConsultantTest extends WebDriverSettings {
+    @Test
+    public void firstTest(){
+
+    }
+
+//
+//    driver ("http://www.consultant.ru/cons/");
+//
+//    WebElement searchPlus = driver.findElement(By.xpath("//a[@menu='searchPlus']"));
+//        searchPlus.click();
+//
+//    WebElement dictFilter = driver.findElement(By.xpath("//input[@name='dictFilter']"));
+//        dictFilter.sendKeys("Налоговый часть");
+//
+//    WebElement runSearchFlat = driver.findElement(By.xpath("//button[@class='runsearch flat']"));
+//        runSearchFlat.click();
+//
+//    List<WebElement> searchResults= driver.findElements(By.xpath("//a[@class='search-results__link']"));
+}
